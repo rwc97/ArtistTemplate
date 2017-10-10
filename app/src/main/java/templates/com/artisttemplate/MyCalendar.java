dda8905e0f0ede44318517e3b32aac9062a14cd4
Ryan Curran <rwc24@pitt.edu>
2017-10-09 21:12:22 -0400

    Initial commit

---
+++ app/src/main/java/templates/com/artisttemplate/MyCalendar.java
@@ -0,0 +1,40 @@
+package templates.com.artisttemplate;
+
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Calendar;
+
+
+public class MyCalendar extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        //Toast toast = null;
+        //toast.makeText(getApplicationContext(), "HELLO", Toast.LENGTH_SHORT).show();
+
+
+    }
+}
